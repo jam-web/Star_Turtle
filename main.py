e0c809dc115c54818421e96d41c5b39ad0299a3d
Judith Magee <Judith783@gmail.com>
2021-07-07 22:26:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import turtle as turtle
+
+turtle.pencolor("orange")
+turtle.forward(110)
+turtle.left(216)
+
+turtle.pencolor("green")
+turtle.forward(110)
+turtle.left(216)
+
+turtle.pencolor("yellow")
+turtle.forward(110)
+turtle.left(216)
+
+turtle.pencolor("blue")
+turtle.forward(110)
+turtle.left(216)
+
+turtle.pencolor("red")
+turtle.forward(110)
+turtle.right(216)
+
+
+
